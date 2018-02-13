4dc61385cf3a18444d25ebf3c37b5758d195a010
Seth Reid <contact@sethreid.co.nz>
2018-02-13 21:05:34 +1300

    finished step

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
-import { getEthereumData, getMicrosoftData } from './api/index';
+import { getEthereumDataFromApi, getMicrosoftDataFromApi } from './api/index';
 import { PriceTable } from "./components/PriceTable";
-
-const ethereumData = getEthereumData();
-const microsoftData = getMicrosoftData();
+import { Spinner } from "./components/Spinner";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ethereumData: null,
+      microsoftData: null,
+      hasLoaded: false
+    }
+  }
+  async componentDidMount() {
+    const ethereumData = await getEthereumDataFromApi();
+    const microsoftData = await getMicrosoftDataFromApi();
+    this.setState({
+      ethereumData,
+      microsoftData,
+      hasLoaded: true
+    })
+  }
   render() {
+    const { ethereumData, microsoftData, hasLoaded } = this.state;
+    if (!hasLoaded) return <Spinner />
     return (
       <div className="crypto-stock-compare">
         <h1>Crypto Stock Compare</h1>
+        {}
         <section className="value-table">
           <h2>Ethereum</h2>
           <PriceTable priceData={ethereumData} />
---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Spinner.css';
+
+export const Spinner = () => (
+  <div class="spinner">
+    <div class="bounce1" />
+    <div class="bounce2" />
+    <div class="bounce3" />
+  </div>
+);
