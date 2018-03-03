47a48324ddc398dfb6ff88e51bf6479b063b1ef5
Seth Reid <contact@sethreid.co.nz>
2018-03-03 18:57:28 +1300

    add loader

---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Loader.css';
+
+export const Loader = () => (
+  <div class="loader">
+    <div class="bounce1" />
+    <div class="bounce2" />
+    <div class="bounce3" />
+  </div>
+);
